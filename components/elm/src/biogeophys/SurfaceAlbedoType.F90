9bc9d07beeb9e5b836092590ffdc58d6cd88740a
donghuix <donghui.xu@pnnl.gov>
2021-02-24 14:35:38 -0800

    debug

--- components/elm/src/biogeophys/SurfaceAlbedoType.F90
+++ components/elm/src/biogeophys/SurfaceAlbedoType.F90
@@ -6,6 +6,7 @@ module SurfaceAlbedoType
   use shr_kind_mod   , only : r8 => shr_kind_r8
   use shr_log_mod    , only : errMsg => shr_log_errMsg
   use decompMod      , only : bounds_type
+  use elm_varcon     , only : grlnd
   use elm_varpar     , only : numrad, nlevcan, nlevsno
   use elm_varctl     , only : fsurdat, iulog
   use abortutils     , only : endrun
