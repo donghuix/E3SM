27b4692154acf97e337e6b9bcbaf0753695474ca
Donghui Xu <donghui.xu@pnnl.gov>
2022-11-14 11:08:40 -0800

    ocn to lnd one way coupling continue
    Conflicts:
    	components/data_comps/docn/src/docn_comp_mod.F90
    	components/elm/src/biogeophys/BalanceCheckMod.F90
    	components/elm/src/biogeophys/SoilHydrologyMod.F90
    	components/elm/src/biogeophys/SoilHydrologyType.F90
    	components/elm/src/data_types/ColumnDataType.F90

--- components/data_comps/docn/src/docn_comp_mod.F90
+++ components/data_comps/docn/src/docn_comp_mod.F90
@@ -69,7 +69,7 @@ module docn_comp_mod
   real(R8),parameter :: latice  = shr_const_latice      ! latent heat of fusion
   real(R8),parameter :: ocnsalt = shr_const_ocn_ref_sal ! ocean reference salinity
 
-  integer(IN)   :: kt,ks,ku,kv,kdhdx,kdhdy,kq,kswp,kh2o ! field indices
+  integer(IN)   :: kt,ks,ku,kv,kdhdx,kdhdy,kq,kswp,kssh,kh2o ! field indices
   integer(IN)   :: kswnet,klwup,klwdn,ksen,klat,kmelth,ksnow,krofi
   integer(IN)   :: kh,kqbot,kfraz
   integer(IN)   :: k10uu           ! index for u10
@@ -90,16 +90,17 @@ module docn_comp_mod
 #endif
 
   !--------------------------------------------------------------------------
-  integer(IN)     , parameter :: ktrans = 9
+  integer(IN)     , parameter :: ktrans = 10
   character(14)   , parameter :: avifld(1:ktrans) = &
        (/ "t             ","u             ","v             ","dhdx          ",&
           "dhdy          ","s             ","h             ","qbot          ",&
-          "frac_h2oocn   "/)
+          "ssh           ","frac_h2oocn   "/)
   character(14)   , parameter  :: avofld(1:ktrans) = &
        (/ "So_t          ","So_u          ","So_v          ","So_dhdx       ",&
           "So_dhdy       ","So_s          ","strm_h        ","strm_qbot     ",&
-          "So_frac_h2oocn"/)
+          "So_ssh        ","So_frac_h2oocn"/)
   character(len=*),parameter :: flds_strm = 'strm_h:strm_qbot:So_t'
+
   !--------------------------------------------------------------------------
 
   !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -291,6 +292,7 @@ CONTAINS
     kswp  = mct_aVect_indexRA(o2x,'So_fswpen', perrwith='quiet')
     kq    = mct_aVect_indexRA(o2x,'Fioo_q') ! ocn freezing melting potential
     kfraz = mct_aVect_indexRA(o2x,'Fioo_frazil') ! ocn frazil
+    kssh  = mct_aVect_indexRa(o2x,'So_ssh', perrwith='quiet')
     kh2o  = mct_aVect_indexRA(o2x,'So_frac_h2oocn', perrwith='quiet')
 
     call mct_aVect_init(x2o, rList=seq_flds_x2o_fields, lsize=lsize)
@@ -649,6 +651,9 @@ CONTAINS
 ! make sure frazil is 0. MPAS-seaice will still use it.
        o2x%rAttr(kfraz,n) = 0.0_R8
        if (kh2o /= 0) then
+         o2x%rAttr(kssh, n) = 0.0_R8
+       endif
+       if (kh2o /= 0) then
          o2x%rAttr(kh2o, n) = 0.0_R8
        endif
        if (kswp /= 0) then
@@ -905,6 +910,9 @@ CONTAINS
           o2x%rAttr(kdhdx,n) = 0.0_R8
           o2x%rAttr(kdhdy,n) = 0.0_R8
           o2x%rAttr(kq   ,n) = 0.0_R8
+          if (kssh /= 0) then
+            o2x%rAttr(kssh, n) = o2x%rAttr(kssh, n)
+          endif
           if (kh2o /= 0) then
              if (o2x%rAttr(kh2o, n) < 0.0_R8) then
                 o2x%rAttr(kh2o, n) = 0.0_R8 ! Inundation cannot be negative
--- components/elm/src/main/elm_driver.F90
+++ components/elm/src/main/elm_driver.F90
@@ -1221,7 +1221,7 @@ contains
             filter(nc)%num_hydrononsoic, filter(nc)%hydrononsoic, &
             filter(nc)%num_urbanc, filter(nc)%urbanc,             &
             filter(nc)%num_do_smb_c, filter(nc)%do_smb_c,         &
-            atm2lnd_vars, glc2lnd_vars,        &
+            atm2lnd_vars, glc2lnd_vars, ocn2lnd_vars,             &
             soilhydrology_vars, soilstate_vars)
 
        else
@@ -1231,7 +1231,7 @@ contains
             filter(nc)%num_hydrologyc, filter(nc)%hydrologyc, &
             filter(nc)%num_urbanc, filter(nc)%urbanc,         &
             filter(nc)%num_do_smb_c, filter(nc)%do_smb_c,     &
-            atm2lnd_vars, glc2lnd_vars,      &
+            atm2lnd_vars, glc2lnd_vars, ocn2lnd_vars,         &
             soilhydrology_vars, soilstate_vars)
 
        end if
